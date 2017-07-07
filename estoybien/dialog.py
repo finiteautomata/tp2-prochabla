3c29367aa9a50896f164413411279528abae8b16
Juan Manuel Pérez <jmperez.85@gmail.com>
2017-07-07 20:02:04 -0300

    Muevo a utils.

--- estoybien/dialog.py
+++ estoybien/dialog.py
@@ -1,7 +1,6 @@
 #! coding: utf-8
 u"""Clase que implementa el diálogo con una persona."""
-from tempfile import NamedTemporaryFile
-from pydub import AudioSegment
+from . import utils
 
 
 class Dialog(object):
@@ -38,23 +37,8 @@ class Dialog(object):
 
     def voice_received(self, bot, update):
         u"""Acción a realizar al recibir un archivo de voz."""
-        wav_file = self._save_to_wav(bot, update)
+        wav_file = utils.save_to_wav(bot, update)
 
         print("Archivo guardado en {}".format(wav_file.name))
 
         bot.send_message(chat_id=update.message.chat_id, text="Recibí audio")
-
-    def _save_to_wav(self, bot, update):
-        """Salva update de audio en un .wav."""
-        file_id = update.message.voice.file_id
-        ext = update.message.voice.mime_type.split("/")[-1]
-        temp_file = NamedTemporaryFile(suffix=".{}".format(ext), delete=False)
-        wav_file = NamedTemporaryFile(suffix=".wav", delete=False)
-
-        # Primero la guardo en el formato que venga
-        voice_file = bot.get_file(file_id)
-        voice_file.download(temp_file.name)
-        # Luego la convierto a .wav
-        AudioSegment.from_file(temp_file.name).export(wav_file, format="wav")
-
-        return wav_file
---
+++ estoybien/utils.py
@@ -0,0 +1,20 @@
+#! coding: utf-8
+u"""Módulo para utilidades varias de estoybien."""
+from tempfile import NamedTemporaryFile
+from pydub import AudioSegment
+
+
+def save_to_wav(bot, update):
+    """Salva update de audio en un .wav."""
+    file_id = update.message.voice.file_id
+    ext = update.message.voice.mime_type.split("/")[-1]
+    temp_file = NamedTemporaryFile(suffix=".{}".format(ext), delete=False)
+    wav_file = NamedTemporaryFile(suffix=".wav", delete=False)
+
+    # Primero la guardo en el formato que venga
+    voice_file = bot.get_file(file_id)
+    voice_file.download(temp_file.name)
+    # Luego la convierto a .wav
+    AudioSegment.from_file(temp_file.name).export(wav_file, format="wav")
+
+    return wav_file
